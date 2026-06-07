4f2d0c727d486b83295d382268556de8c06c4469
mohak <mohakchoubey@gmail.com>
2026-06-07 20:49:58 +0530

    Update firebase-auth.js

--- firebase-auth.js
+++ firebase-auth.js
@@ -8,7 +8,13 @@ import {
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-   // your config
+  apiKey: "AIzaSyAeHDhdiTRwftUGYgrb1m19v7hm2R5rb-Y",
+  authDomain: "toolbox-hub-98c03.firebaseapp.com",
+  projectId: "toolbox-hub-98c03",
+  storageBucket: "toolbox-hub-98c03.firebasestorage.app",
+  messagingSenderId: "321020105472",
+  appId: "1:321020105472:web:356e7c2903def7b9d859e5"
+   
 };
 
 const app = initializeApp(firebaseConfig);
